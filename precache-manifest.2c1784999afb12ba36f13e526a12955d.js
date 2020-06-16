8bb3b44a5b35ba2b038adb1a09e02bc125601248
ascode94 <ascode94@gmail.com>
2020-06-16 13:46:25 +0430

    Updates

---
+++ precache-manifest.2c1784999afb12ba36f13e526a12955d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67719de6636728cc5da03f2fcbeb54fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa5fb280910b5224a4c",
+    "url": "/static/css/main.96f60ae9.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa5fb280910b5224a4c",
+    "url": "/static/js/main.f4d9e681.chunk.js"
+  },
+  {
+    "revision": "321be40e4d0b3008f64a",
+    "url": "/static/js/runtime-main.c34bb27e.js"
+  }
+]);
